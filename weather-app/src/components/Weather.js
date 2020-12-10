ce72f5b6f6e610a301f4f50c6d2fb00d9f75f5af
Liam <muirliam23@gmail.com>
2020-12-10 12:03:22 -0500

    added api script for todays weather

--- weather-app/src/components/Weather.js
+++ weather-app/src/components/Weather.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const Weather = ({page,setPage}) => {
+const Weather = ({page,setPage,weather}) => {
 
     const buttonHandler = (a) => {
         setPage("home")
+    }
+    const temp = () => {
+        if (typeof(weather) !== 'undefined'){
+            return (Number(weather.data.main.temp) - 273.15).toFixed(2)
+        }
     }
 
     return (
@@ -11,7 +16,9 @@ const Weather = ({page,setPage}) => {
             <button className="link" onClick={buttonHandler}>Back to Home</button>
             <div className="todaysWeather">
                 <h2>Today's Weather</h2>
-                <p>Quite Sunny</p>
+                <p>Temperature: {temp()} C</p>
+                <p>Weather: {weather.data.weather[0].description}</p>
+                <p>Cloudiness: {weather.data.clouds.all}%</p>
             </div>
         </div>
     )
