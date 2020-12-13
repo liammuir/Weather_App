cfaee311f592f0d92f02696bd7bf162a586e1023
Liam <muirliam23@gmail.com>
2020-12-13 12:37:34 -0500

    now makes yesterday weather api calls

--- weather-app/src/components/Weather.js
+++ weather-app/src/components/Weather.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const Weather = ({page,setPage,weather}) => {
-    let isnoweather = weather === 0
+    let isnoweather = weather === 0 || weather.config.url.search("data/2.5/weather") === -1
+    
     const temp = () => {
-        if (weather !== 0){
+        if (isnoweather === false){
             console.log(weather)
             return (Number(weather.data.main.temp) - 273.15).toFixed(2)
         }
@@ -14,7 +15,7 @@ const Weather = ({page,setPage,weather}) => {
             <div className="todaysWeather">
                 <h2>Today's Weather</h2>
                 <p>Temperature: {temp()} C</p>
-                <p>Weather: {isnoweather ? <></> : weather.data.weather[0].description}</p>
+                <p>Weather: {isnoweather ? <></> : weather.data.weather.map((r) => r.description + " ")}</p>
                 <p>Cloudiness: {isnoweather ? <></> : weather.data.clouds.all}%</p>
             </div>
         </div>
